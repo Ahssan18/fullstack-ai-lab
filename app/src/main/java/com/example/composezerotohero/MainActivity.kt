5fc7048394660de6d8f4b33e6a6234dcc42d663e
Ahssan18 <ahssanakhtar18@gmail.com>
2026-08-14 17:43:09 +0500

    feat(login): cleanup redundant code and prepare for MVI implementation

--- app/src/main/java/com/example/composezerotohero/MainActivity.kt
+++ app/src/main/java/com/example/composezerotohero/MainActivity.kt
@@ -4,51 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.automirrored.filled.NavigateNext
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.DarkMode
-import androidx.compose.material.icons.filled.Info
-import androidx.compose.material.icons.filled.Lock
-import androidx.compose.material.icons.filled.Notifications
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material.icons.filled.StarRate
-import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
 import com.example.composezerotohero.ui.app.MyApp
 import com.example.composezerotohero.ui.theme.ComposeZeroToHeroTheme
+
 @ExperimentalMaterial3Api
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/composezerotohero/ui/presentation/login/Login.kt
+++ app/src/main/java/com/example/composezerotohero/ui/presentation/login/Login.kt
@@ -1,18 +1,8 @@
 package com.example.composezerotohero.ui.presentation.login
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 
 @Composable
-fun LoginScreen(){
-    Column {
-
-    }
-}
-
-
-@Composable
-fun CommonTextField(){
-    TextField(value = )
+fun LoginScreen() {
+    // To be implemented
 }
